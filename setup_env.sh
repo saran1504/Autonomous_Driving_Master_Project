7f0f472bb64a1c70358a73b50e2006fd1a8a5d64
saran1504 <84578097+saran1504@users.noreply.github.com>
2022-11-22 01:15:39 +0100

    init

---
+++ setup_env.sh
@@ -0,0 +1,16 @@
+echo "Installing dependencies..."
+
+# install anything...
+ yes | pip3 install mean-average-precision
+ yes | pip3 install numpy
+ yes | pip3 install bounding_box
+ pip3 install --no-input torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+ yes | pip3 install pyyaml
+ yes | pip3 install webcolors
+ yes | pip3 install matplotlib
+ yes | pip3 install webcolors
+ yes | pip3 install visdom
+ yes | pip3 install tqdm
+ yes | pip3 install opencv-python
+ yes | pip3 install torchvision
+# install anything...
